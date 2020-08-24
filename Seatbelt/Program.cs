8cd7d2e66c7af33090aa90c3d6ca330e56f0f574
jokercygnus <66358747+jokercygnus@users.noreply.github.com>
2020-08-24 15:48:03 +0300

    Update Program.cs
    Change Main function from private to public.
    For PowerShell assembly function calls.

--- Seatbelt/Program.cs
+++ Seatbelt/Program.cs
@@ -2,7 +2,7 @@
 {
     public static class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
             (new Seatbelt(args)).Start();
         }
